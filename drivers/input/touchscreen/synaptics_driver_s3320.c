38ba1259438e6c295df79f1058495d3eaa8df429
Steve Kondik <steve@cyngn.com>
2016-07-20 12:05:43 -0700

    input: synaptics_s3320: Report nanosecond timestamps to improve accuracy
    Change-Id: Ia6f96dafcd4fa3c3938530a865b08dc1b07fc57e
    Signed-off-by: TheDemon <kartikbhalla12@gmail.com>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -482,6 +482,8 @@ struct synaptics_ts_data {
 #ifdef SUPPORT_VIRTUAL_KEY
         struct kobject *properties_kobj;
 #endif
+
+	ktime_t timestamp;
 };
 
 static struct device_attribute attrs_oem[] = {
@@ -1327,6 +1329,11 @@ void int_touch(void)
 		goto INT_TOUCH_END;
 	}
 
+	input_event(ts->input_dev, EV_SYN, SYN_TIME_SEC,
+			ktime_to_timespec(ts->timestamp).tv_sec);
+	input_event(ts->input_dev, EV_SYN, SYN_TIME_NSEC,
+			ktime_to_timespec(ts->timestamp).tv_nsec);
+
 	for( i = 0; i < count_data; i++ ) {
 		points.status = buf[i*8];
 		points.x = ((buf[i*8+2]&0x0f)<<8) | (buf[i*8+1] & 0xff);
@@ -1474,10 +1481,9 @@ static enum hrtimer_restart synaptics_ts_timer_func(struct hrtimer *timer)
 static irqreturn_t synaptics_irq_thread_fn(int irq, void *dev_id)
 {
 	struct synaptics_ts_data *ts = (struct synaptics_ts_data *)dev_id;
-	mutex_lock(&ts->mutex);
-    touch_disable(ts);
-	queue_work(synaptics_report, &ts->report_work);
-	mutex_unlock(&ts->mutex);
+	ts->timestamp = ktime_get();
+	touch_disable(ts);
+	synaptics_ts_work_func(&ts->report_work);
 	return IRQ_HANDLED;
 }
 #endif
