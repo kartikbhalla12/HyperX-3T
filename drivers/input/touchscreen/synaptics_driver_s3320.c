6eed0ac209bb4e4e0735c0b5f39736caba04258b
TheDemon <kartikbhalla12@gmail.com>
2017-07-13 11:40:00 +0000

    Revert "synaptics_s3320: remove unnecessary mutex locks inside IRQ handler"
    This reverts commit 47a9b09445bc3c7a123eea8cd95ce25f39712747.

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -1464,10 +1464,10 @@ static enum hrtimer_restart synaptics_ts_timer_func(struct hrtimer *timer)
 static irqreturn_t synaptics_irq_thread_fn(int irq, void *dev_id)
 {
 	struct synaptics_ts_data *ts = (struct synaptics_ts_data *)dev_id;
-
-	touch_disable(ts);
+	mutex_lock(&ts->mutex);
+    touch_disable(ts);
 	queue_work(synaptics_report, &ts->report_work);
-
+	mutex_unlock(&ts->mutex);
 	return IRQ_HANDLED;
 }
 #endif
